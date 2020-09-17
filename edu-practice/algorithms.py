dbbed182fa79ec7b9b9ac6e7f62294b2df0cad71
Arthur Bondar <olemannen577@gmail.com>
2020-09-17 12:12:30 +0300

    added decent data validation

--- edu-practice/algorithms.py
+++ edu-practice/algorithms.py
@@ -1,6 +1,34 @@
 # Calculate the number of ways in which a bunny with a maximal step of K 
 # can get on Nth stair
 
+def validateAndRun():
+    validationFlag = False
+    while not validationFlag:
+        try: 
+            n = int(input('Enter N value: '))
+            print('-' * 90)
+            k = int(input('Enter K value: '))
+            print('-' * 90)
+            validationFlag = True
+
+            if not (1 <= n and n <= 300):
+                print('N value must be within [1, 300]')
+                print('-' * 90)
+                validationFlag = False
+            if not (1 <= k and k <= 300 and k <= n):
+                print('K value must be within [1, 300] and less than N')
+                print('-' * 90)
+                validationFlag = False
+
+        except ValueError: 
+            print('-' * 90)
+            print('N and K must have an integer value')
+            print('-' * 90)
+
+    print('You can get on %dth stair with maximal step of %d in %d ways' 
+        % (n, k, calcWays(k, n)))
+
+
 def calcWays(K, N):        
     temp = 0
     res = [1]  
@@ -19,15 +47,8 @@ def calcWays(K, N):
 
     return res[N]  
 
+
 print('-' * 90)
 print('This programm counts the number of ways to get on Nth stair with the maximal step K')
 print('-' * 90)
-n = int(input('Enter N value: '))
-assert 1 <= n and n <= 300
-print('-' * 90)
-k = int(input('Enter K value: '))
-assert 1 <= k and k <= 300 and k <= n
-print('-' * 90)
-
-print('You can get on %dth stair with maximal step of %d in %d ways' 
-    % (n, k, calcWays(k, n)))
+validateAndRun()
