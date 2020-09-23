3ad27a4b410d41b4d9f56745762ef12fa9299560
Arthur Bondar <olemannen577@gmail.com>
2020-09-23 19:46:00 +0300

    added solution for 2nd task edu practice

--- array.py
+++ array.py
@@ -62,6 +62,20 @@ def generateRandom():
             # Programming task
             print('Your list:', lst, '\n')
             print('\nREVERSED LIST -', alternationsReverse(lst))
+
+            # Edu practice task
+            k = input('\nFind all the entries of K value\nEnter K value or type \'q\' to quit: ')
+            if (k == 'q'): break
+            k = int(k)
+
+            indexLst = [i for i in range(0, len(lst))]
+            sort(lst, indexLst)
+
+            sortedIndexes = find(lst, k)
+            print('\n%d is on position' % (k), end=' ')
+            for i in sortedIndexes:
+                print(indexLst[i], end=' ')
+            print('\n')
             break
         except ValueError:
             print('All input data must have an integer value\n')
@@ -78,9 +92,89 @@ def generateFromInput():
             print('Your list:', lst, '\n')
             lst = alternationsReverse(lst)
             print('\nREVERSED LIST -', lst)
+
+            # Edu practice task
+            k = input('\nFind all the entries of K value\nEnter K value or type \'q\' to quit: ')
+            if (k == 'q'): break
+            k = int(k)
+
+            indexLst = [i for i in range(0, len(lst))]
+            sort(lst, indexLst)
+            print('Sorted list:', lst)
+
+            sortedIndexes = find(lst, k)
+            print('\n%d is on position' % (k), end=' ')
+            for i in sortedIndexes:
+                print(indexLst[i], end=' ')
+            print('\n')
             break
         except ValueError:
             print('All input data must have an integer value\n')
             continue
+
+def sort(lst, indLst):
+    """Sorting two lists in terms of the first one"""
+    for i in range(0, len(lst)):
+        for j in range(0, len(lst)-1-i):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+                indLst[j], indLst[j + 1] = indLst[j + 1], indLst[j]
+
+def find(lst, k):
+    first = findFirst(lst, k)
+    last = findLast(lst, k)
+    print('\nAll %ds were found for %d operations' % (k, first[0]+last[0]))
+
+    return list(range(first[1], last[1]+1))
+      
+def findFirst(lst, k):
+    """Binary search algorithm for first K entry in the list"""
+    first = -1
+    low, top = 0, len(lst) - 1
+    print('\nLooking for a first entry of %d' % (k))
+    counter = 1
+    
+    while low <= top: 
+        mid = low + (top - low) // 2
+        
+        print('%d. Checking if element is between pos %d and %d...' % (counter, low, top))
+        counter+=1
+        print('\tChecking if element is on pos %d...' % (mid))
+
+        if (lst[mid] == k):
+            first = mid
+            top = mid - 1
+            print('Found %d on pos %d!' % (k, mid))
+        elif lst[mid] < k: 
+            low = mid + 1
+        else: 
+            top = mid - 1 
+
+    return [counter, first]
+
+def findLast(lst, k):
+    """Binary search algorithm for last K entry in the list"""
+    last = -1
+    low, top = 0, len(lst) - 1
+    print('\nLooking for a last entry of %d' % (k))
+    counter = 1
+
+    while low <= top: 
+        mid = low + (top - low) // 2
+
+        print('%d. Checking if element is between pos %d and %d...' % (counter, low, top))
+        counter+=1
+        print('\tChecking if element is on pos %d...' % (mid))
+
+        if (lst[mid] == k):
+            last = mid
+            low = mid + 1
+            print('Found %d on pos %d!' % (k, mid))
+        elif lst[mid] < k: 
+            low = mid + 1
+        else: 
+            top = mid - 1
+
+    return [counter, last]
 
 menu()
