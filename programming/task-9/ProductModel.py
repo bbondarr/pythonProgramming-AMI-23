9e0e22a5b33cfd1d23519a3f46eadb8f220cb798
Arthur Bondar <olemannen577@gmail.com>
2020-11-24 16:19:03 +0200

    added 'pagination' and 'count' support for Product RESTful API

---
+++ programming/task-9/ProductModel.py
@@ -0,0 +1,76 @@
+import json
+from datetime import date
+
+from app import db
+from Validation import Validation as v
+
+
+class Product(db.Model):
+    __tablename__ = 'Products'
+    iD = db.Column('id', db.Integer, primary_key=True)
+    title = db.Column('title', db.String, nullable=False)
+    imageURL = db.Column('imageURL', db.String, nullable=False)
+    price = db.Column('price', db.Float, nullable=False)
+    createdAt = db.Column('createdAt', db.String, nullable=False)
+    updatedAt = db.Column('updatedAt', db.String, nullable=False)
+    description = db.Column('description', db.Text, nullable=False)
+
+
+    def __init__(self, 
+        title, imageURL, price, createdAt, updatedAt, description, iD=None):
+
+        if iD:  
+            self.iD = str(iD)
+        self.setTitle(title)
+        self.setImageURL(imageURL)
+        self.setPrice(price)
+        self.setDescription(description)
+        self.setCreatedAt(createdAt)
+        self.setUpdatedAt(updatedAt)
+
+        Product.compareDates(self.createdAt, self.updatedAt)
+
+    @staticmethod
+    def attributes():
+        return [a for a in dir(Product) 
+                if (not a.startswith('get') 
+                and not a.startswith('set') 
+                and not a.startswith('_') 
+                and a != 'query' and a != 'metadata'
+                and not callable(getattr(Product, a)))]
+
+    @v.validateTitle
+    def setTitle(self, val): 
+        self.title = val
+        self.updatedAt = date.today()
+    
+    @v.validateURL
+    def setImageURL(self, val):
+        self.imageURL = val
+        self.updatedAt = date.today()
+    
+    @v.validateFloat
+    @v.validateFloatInRange
+    def setPrice(self, val): 
+        self.price = val
+        self.updatedAt = date.today()
+    
+    @v.validateStr
+    def setDescription(self, val): 
+        self.description = val
+        self.updatedAt = date.today()
+
+    @v.validateDate
+    def setCreatedAt(self, val):
+        self.createdAt = val
+        self.updatedAt = date.today()
+    
+    @v.validateDate
+    def setUpdatedAt(self, val):
+        Product.compareDates(self.createdAt, val)
+        self.updatedAt = val
+
+    @staticmethod
+    @v.validateTwoDates
+    def compareDates(date1, date2):
+        pass
---
+++ programming/task-9/apiFeatures.py
@@ -0,0 +1,32 @@
+from sqlalchemy import desc, cast
+
+from app import db
+from ProductModel import Product
+
+def sort(query, sortBy):
+    if sortBy:
+        if sortBy in Product.attributes(): 
+            query = query.order_by(sortBy)
+            
+        if (sortBy[0] == '-' and 
+            sortBy[1:] in Product.attributes()):
+            query = query.order_by(desc(sortBy[1:]))
+
+    return query
+
+
+def filter(query, filterBy):
+    if filterBy:
+        query = query.filter(
+                    Product.title.like('%'+filterBy+'%') |
+                    Product.imageURL.like('%'+filterBy+'%') |
+                    Product.description.like('%'+filterBy+'%') |
+                    cast(Product.iD, db.String).like('%'+filterBy+'%') |
+                    cast(Product.price, db.String).like('%'+filterBy+'%') |
+                    cast(Product.createdAt, db.String).like('%'+filterBy+'%') |
+                    cast(Product.updatedAt, db.String).like('%'+filterBy+'%'))
+    return query
+
+
+def paginate(query, page, per_page):
+    return query.paginate(page, per_page).items
